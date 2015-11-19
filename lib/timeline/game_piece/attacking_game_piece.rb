67d862595df695f1a849f2130f6cc2bf54b43731
Ben <ben@registria.com>
2015-11-18 23:10:02 -0800

    how did specs not catch that...

--- lib/timeline/game_piece/attacking_game_piece.rb
+++ lib/timeline/game_piece/attacking_game_piece.rb
@@ -10,7 +10,7 @@ module AttackingGamePiece
       target.living? and can_attack? target
     end.first
 
-    attack first_target unless dead? or target.nil?
+    attack first_target unless dead? or first_target.nil?
   end
 
   def attack target
