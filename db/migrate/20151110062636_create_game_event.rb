a02b136ba20e34f7fd9d0173814ef087010e733c
Ben <ben@registria.com>
2015-11-18 22:42:21 -0800

    fixes for actual web host, postgres db

--- db/migrate/20151110062636_create_game_event.rb
+++ db/migrate/20151110062636_create_game_event.rb
@@ -3,7 +3,7 @@ class CreateGameEvent < ActiveRecord::Migration
     create_table :game_events do |t|
       t.references :game_event_list, index: true, foreign_key: true
       t.references :player, index: true, foreign_key: true
-      t.boolean :causal, null: false, default: 1
+      t.boolean :causal, null: false, default: true
       t.integer :frame, null: false, limit: 8, index: true
       t.integer :acausal_target_frame, default: nil, limit: 8
 
--- db/migrate/20151111071912_add_require_advance_game_version_to_game.rb
+++ db/migrate/20151111071912_add_require_advance_game_version_to_game.rb
@@ -1,5 +1,5 @@
 class AddRequireAdvanceGameVersionToGame < ActiveRecord::Migration
   def change
-    add_column :games, :require_advance_game_version, :boolean, default: 0, null: false
+    add_column :games, :require_advance_game_version, :boolean, default: false, null: false
   end
 end
